793a0b78737ec0714d08cb44c5d50ab96812dd2e
kungstriving <kongxiaoyangsh@foxmail.com>
2014-11-09 21:39:55 +0800

    dr 修改后可运行

---
+++ DataRunner/WebContent/js/dr/base/ContUnit.js
@@ -0,0 +1,28 @@
+//define the Page module
+
+define(["dojo/_base/declare",
+        "dojo/store/Memory",
+        "dojo/store/Observable",
+        "dr/base/common"],
+	function(declare, Memory, Observable, 
+			common) {
+		return declare(null, {
+			
+			/****************** fields *****************************/
+			node:null,		//NostNode object
+			field:"",		//avoid reference error use null
+			
+			/******************* methods *************************/
+			
+			set:function(newValue) {
+				this.node.set(this.field, newValue);
+//				var fieldFun = this.node["set"+field];
+//				fieldFun(newValue);
+			},
+			
+			constructor:function(pNode, pField) {
+				this.node = pNode;
+				this.field = pField;
+			}
+		});
+});
