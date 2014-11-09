ed31b24b498779aabf5a9bcf5ac6f7b97d75f35e
kongxiaoyang <kongxiaoyang@V0B0-178306-3.ccdomain.com>
2014-11-09 09:33:07 +0800

    init checkin

---
+++ DataRunner/WebContent/studio/editor.js
@@ -0,0 +1,9 @@
+require([
+         "dojo/dom",
+         "dijit/layout/BorderContainer",	
+         "dijit/layout/ContentPane",
+         "dojo/domReady!"
+         ],
+function(dom) {
+	console.log("domready");
+});
